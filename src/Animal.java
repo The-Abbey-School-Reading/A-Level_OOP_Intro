dc61424e0494cf80be4354a7d416397c1037fb97
Mr-Bradley-TheAbbey <bradleyro@theabbey.co.uk>
2024-09-18 14:00:26 +0100

    First commit

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+
+    public String name;
+    public int noOfLegs;
+    public String noise;
+
+    public Animal(String name, int number, String sound){
+        this.name = name;
+        this.noOfLegs = number;
+        this.noise = sound;
+
+        System.out.printf("Animal object created for %s. This animal has %d legs. It goes %s!", this.name, this.noOfLegs, this.noise);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Testing from Scratch");
+        Animal cow = new Animal("cow", 4, "moo");
+    }
+}
